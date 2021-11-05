285a7cb7e36957e33afb7fa175ae6efc8f4c09f0
san <1412346241@qq.com>
2021-11-05 14:14:34 +0800

    1105-14:14-office-pc-commit

--- pkg/agent/get_metadata.go
+++ pkg/agent/get_metadata.go
@@ -1,44 +1,17 @@
 package agent
 
 import (
-	"net"
-	"net/http"
-	"io/ioutil"
 	"github.com/go-kit/kit/log"
 	"github.com/go-kit/kit/log/level"
+	"net"
 )
 
 var (
 	LocalRegion string
 	LocalIp     string
 )
+
 // TODO get real id func
-
-func GetLocalRegionByEc2(logger log.Logger) bool {
-	addr := "http://169.254.169.254/latest/meta-data/placement/availability-zone"
-	req, err := http.NewRequest("GET", addr, nil)
-	var resp *http.Response
-	resp, err = http.DefaultClient.Do(req)
-	if err != nil {
-		level.Error(logger).Log("msg", "GetLocalRegionByEc2_http_error1", "error", err)
-		return false
-	}
-	if resp.StatusCode != http.StatusOK {
-		level.Error(logger).Log("msg", "GetLocalRegionByEc2_http_error_rc_ne_200", "resp.StatusCode", resp.StatusCode)
-		return false
-	}
-
-	respBytes, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		level.Error(logger).Log("msg", "GetLocalRegionByEc2_http_error_read_response_body", "err", err)
-		return false
-	}
-	defer resp.Body.Close()
-	dataStr := string(respBytes)
-	region := dataStr[:len(dataStr)-1]
-	LocalRegion = region
-	return true
-}
 
 func GetLocalIp(logger log.Logger) bool {
 
@@ -59,4 +32,33 @@ func GetLocalIp(logger log.Logger) bool {
 	}
 	return false
 
+}
+
+func GetLocalRegionByEc2(logger log.Logger) bool {
+	/*
+		addr := "http://169.254.169.254/latest/meta-data/placement/availability-zone"
+		req, err := http.NewRequest("GET", addr, nil)
+		var resp *http.Response
+		resp, err = http.DefaultClient.Do(req)
+		if err != nil {
+			level.Error(logger).Log("msg", "GetLocalRegionByEc2_http_error1", "error", err)
+			return false
+		}
+		if resp.StatusCode != http.StatusOK {
+			level.Error(logger).Log("msg", "GetLocalRegionByEc2_http_error_rc_ne_200", "resp.StatusCode", resp.StatusCode)
+			return false
+		}
+
+		respBytes, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			level.Error(logger).Log("msg", "GetLocalRegionByEc2_http_error_read_response_body", "err", err)
+			return false
+		}
+		defer resp.Body.Close()
+		dataStr := string(respBytes)
+		region := dataStr[:len(dataStr)-1]
+		LocalRegion = region
+	*/
+	LocalRegion = LocalIp
+	return true
 }
